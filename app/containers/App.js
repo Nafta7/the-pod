7637256ed614f6ebc72a7ac2d77d2778627259e1
Nefla <neflasan@gmail.com>
2016-11-25 13:39:05 -0200

    Add App container

---
+++ app/containers/App.js
@@ -0,0 +1,110 @@
+import { h, Component } from 'preact';
+import AppConstants from '../constants/AppConstants'
+import Constants from '../constants/Constants'
+const config = require('../../appconfig')
+let getLatest, getByDate
+
+if (config.mode === AppConstants.MODE_DEV) {
+  getLatest = require('../../test/api_fixture').getLatest
+  getByDate = require('../../test/api_fixture').getByDate
+} else {
+  getLatest = require('../helpers/api').getLatest
+  getByDate = require('../helpers/api').getByDate
+}
+
+import Nav from '../components/Nav'
+import Loading from '../components/Loading'
+
+import yesterday from '../helpers/yesterday'
+import normalizeDate from '../helpers/normalize-date'
+import randomDate from '../helpers/random-date'
+import displayDate from '../helpers/display-date'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isLoading: true,
+      tries: 0
+    }
+
+    this.handlePreviousClick = this.handlePreviousClick.bind(this)
+    this.handleRandomClick = this.handleRandomClick.bind(this)
+    this.receive = this.receive.bind(this)
+  }
+
+  receive(data){
+    let date = normalizeDate(new Date(data.date))
+    this.setState({
+      image: data.url,
+      image_hd: data.hdurl,
+      title: data.title,
+      explanation: data.explanation,
+      date: date,
+      isLoading: false,
+      tries: 0
+    })
+  }
+
+  makeRequest(date){
+    getByDate(date)
+    .then(this.receive)
+    .catch(err => {
+      this.setState({
+        tries: this.state.tries + 1
+      }, this.makeRequest(yesterday(date)))
+    })
+  }
+
+  handlePreviousClick(){
+    this.setState({
+      isLoading: true
+    }, this.makeRequest(yesterday(this.state.date)))
+
+  }
+
+  handleRandomClick(){
+    this.setState({
+      isLoading: true
+    }, this.makeRequest(randomDate(this.state.date)))
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <Loading />
+      )
+    } else {
+      return (
+        <div>
+          <Nav onPreviousClick={this.handlePreviousClick}
+               onRandomClick={this.handleRandomClick} />
+
+          <div class="image-container">
+            <div class="center">
+              <img src={`${this.state.image_hd}`} class="image" />
+            </div>
+
+            <div>
+              <h1>{this.state.title}</h1>
+              <p>
+                {this.state.explanation}
+              </p>
+              <p>
+                {displayDate(this.state.date)}
+              </p>
+            </div>
+          </div>
+
+        </div>
+      )
+    }
+  }
+
+  componentWillMount(){
+    let today = normalizeDate(Constants.INITIAL_DAY)
+    this.makeRequest(today)
+  }
+}
+
+export default App
