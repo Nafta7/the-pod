256db7b53354ede7afb61bfea3de56c1f6b90cb3
Nefla <neflasan@gmail.com>
2016-11-25 13:37:33 -0200

    Add appconfig and constants

---
+++ app/constants/AppConstants.js
@@ -0,0 +1,7 @@
+const AppConstants = {
+  MODE_DEV: "dev",
+  MODE_PROD: "prod",
+  DEV_TIMEOUT: 200
+}
+
+export default AppConstants
---
+++ app/constants/Constants.js
@@ -0,0 +1,5 @@
+const Constants = {
+  INITIAL_DAY: new Date('2016-11-13')
+}
+
+export default Constants
