02043c7c162bd9dd0d82dc6de8ebcffc969a5b7c
Nefla <neflasan@gmail.com>
2016-11-25 14:50:33 -0200

    Add new Constants

--- app/constants/AppConstants.js
+++ app/constants/AppConstants.js
@@ -1,7 +1,8 @@
 const AppConstants = {
   MODE_DEV: "dev",
   MODE_PROD: "prod",
-  DEV_TIMEOUT: 200
+  DEV_TIMEOUT: 200,
+  TEST_FAILURE: false
 }
 
 export default AppConstants
--- app/constants/Constants.js
+++ app/constants/Constants.js
@@ -1,5 +1,6 @@
 const Constants = {
-  INITIAL_DAY: new Date('2016-11-13')
+  INITIAL_DAY: new Date('2016-11-13'),
+  MAX_TRY: 3
 }
 
 export default Constants
