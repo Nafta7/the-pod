6e604a456d03efa84c5c3bb7fb807647636a5c12
Nefla <neflasan@gmail.com>
2017-02-28 12:07:14 -0300

    Properly center icons

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -27,74 +27,53 @@ const Nav = (props) => {
         <button type="submit" class="btn"
           disabled={disablePrevious}
           onClick={props.onHomeClick}>
-          <div class="btn-container">
-            <span class="btn-icon">
-              <HomeIcon />
-            </span>
-            <span class="btn-text">
-              The Pod
-            </span>
-          </div>
+          <HomeIcon />
+          <span class="btn-text">
+            The Pod
+          </span>
         </button>
       </li>
+
       <li>
         <button
           type="submit" class="btn"
           disabled={disablePrevious}
           onClick={props.onPreviousClick}>
-
-          <div class="btn-container">
-          <span class="btn-icon">
-            <PreviousIcon />
-          </span>
+          <PreviousIcon />
           <span class="btn-text">
             Previous
           </span>
-          </div>
         </button>
       </li>
       <li>
         <button type="submit" class="btn"
           onClick={props.onShuffleClick}>
-          <div class="btn-container">
-            <span class="btn-icon">
-              <ShuffleIcon />
-            </span>
-            <span class="btn-text">
-              Shuffle
-            </span>
-          </div>
-          </button>
+          <ShuffleIcon />
+          <span class="btn-text">
+            Shuffle
+          </span>
+        </button>
       </li>
+
       <li>
         <button type="submit" class="btn"
           disabled={disableNext}
           onClick={props.onNextClick}>
-
-          <div class="btn-container">
-            <span class="btn-icon">
-              <NextIcon />
-            </span>
-            <span class="btn-text">
-              Next
-            </span>
-          </div>
-
+          <NextIcon />
+          <span class="btn-text">
+            Next
+          </span>
         </button>
       </li>
+
       <li>
         <button type="submit"
           class={toggleButtonClasses.join(' ')}
           onClick={props.onToggleClick}>
-
-          <div class="btn-container">
-            <span class="btn-icon">
-              <InfoIcon />
-            </span>
-            <span class="btn-text">
-              Info
-            </span>
-          </div>
+          <InfoIcon />
+          <span class="btn-text">
+            Info
+          </span>
         </button>
       </li>
     </ul>
