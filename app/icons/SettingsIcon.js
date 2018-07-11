ce2bb537bee3081639167c53f3e7b9e9b9bcc4f8
nefla <neflasan@gmail.com>
2018-07-10 23:28:30 -0300

    Add Settings Icon

---
+++ app/icons/SettingsIcon.js
@@ -0,0 +1,12 @@
+import { h } from 'preact'
+
+function SettingsIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24">
+      <path d="M0 0h24v24H0z" fill="none"/>
+      <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/>
+    </svg>
+  )
+}
+
+export default SettingsIcon
